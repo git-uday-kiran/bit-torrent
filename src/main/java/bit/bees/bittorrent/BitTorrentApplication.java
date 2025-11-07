a51f8ca39c6717448eba77d9ec85a28e55272f7f
uday-kiran-mekala <uday.kiran1@gmail.com>
2025-11-07 19:19:02 +0530

    initial commit

---
+++ src/main/java/bit/bees/bittorrent/BitTorrentApplication.java
@@ -0,0 +1,13 @@
+package bit.bees.bittorrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitTorrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitTorrentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bit/bees/bittorrent/BitTorrentApplicationTests.java
@@ -0,0 +1,13 @@
+package bit.bees.bittorrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitTorrentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
